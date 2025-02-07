132fd646c7a9530ab475a5177bb058a44e5b3f30
vot <valentinaorjutapia@gmail.com>
2025-02-07 18:04:03 -0500

    paginas

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ function playSong(songPath) {
     audioPlayer.play();
     alert("Reproduciendo la canción...");
 }
+document.addEventListener("DOMContentLoaded", function() {
+    const videoFrame = document.getElementById("videoFrame");
+
+    if (!videoFrame) {
+        console.error("Elemento 'videoFrame' no encontrado en el DOM.");
+        return; // Detiene la ejecución si no encuentra el elemento
+    }
+
+    const songs = document.querySelectorAll(".song-item");
+
+    songs.forEach(song => {
+        song.addEventListener("click", function() {
+            const videoUrl = this.getAttribute("data-video");
+            videoFrame.src = videoUrl;
+        });
+    });
+});
+
